6fc6463e21bfa036de22d8e8008f120eea0f0949
jwchoi <jwchoi42@naver.com>
2024-07-25 08:16:01 +0900

    test:

--- src/main/java/dev/example/gitstudy/GitStudyApplication.java
+++ src/main/java/dev/example/gitstudy/GitStudyApplication.java
@@ -8,6 +8,8 @@ public class GitStudyApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("");
+        System.out.println("Spring Boot");
         SpringApplication.run(GitStudyApplication.class, args);
     }
 
---
+++ src/main/java/dev/example/gitstudy/Member.java
@@ -0,0 +1,4 @@
+package dev.example.gitstudy;
+
+public class Member {
+}
